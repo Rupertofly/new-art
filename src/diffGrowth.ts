54425608d21dc2cab3fadc7416063ef616871259
Ruby Quail <ruby@rubyquail.design>
2021-09-16 16:20:22 +1000

    boom
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/diffGrowth.ts
+++ src/diffGrowth.ts
@@ -1,30 +1,56 @@
-import { rndm, Vec, Vp } from '@rupertofly/h';
+import { PI, rndm, Vec, Vp } from '@rupertofly/h';
+import { polygonContains } from 'd3-polygon';
+import { quadtree, Quadtree, QuadtreeLeaf } from 'd3-quadtree';
 function limitPure(vec: Vec, scaler: number): Vec {
   if (vec.magnitude() > scaler) {
     return vec.clone().norm().mulScaler(scaler);
   } else return vec.clone();
 }
-const MAX_FORCE = 0.3;
-const MAX_SPEED = 1.4;
-const DESIRED_SEPARATION = 38;
-const COHESION_RATIO = 1.4;
-const MAX_EDGE_LEN = 13;
-
+const MAX_FORCE = 0.2;
+const MAX_SPEED = 0.6;
+const DESIRED_SEPARATION = 10;
+const COHESION_RATIO = 1.1;
+const MAX_EDGE_LEN = 8;
+const DES_SEP_SQ = Math.pow(DESIRED_SEPARATION, 2);
+function search(quadtree: Quadtree<Node>, xmin: number, ymin: number, xmax: number, ymax: number) {
+  const results: Node[] = [];
+  quadtree.visit((node, x1, y1, x2, y2) => {
+    if (!node.length) {
+      let d = (node as QuadtreeLeaf<Node>).data;
+      let x = d.position.x;
+      let y = d.position.y;
+      if (x >= xmin && x < xmax && y >= ymin && y < ymax) {
+        results.push(d);
+      }
+    }
+    return x1 >= xmax || y1 >= ymax || x2 < xmin || y2 < ymin;
+  });
+  return results;
+}
 export class Node {
   position: Vec;
   velocity: Vec;
   acceleration: Vec;
   constructor(x: number, y: number) {
     this.acceleration = new Vec([0, 0]);
-    this.velocity = new Vec([rndm() - 0.5, rndm() - 0.5]).norm();
+    this.velocity = new Vec([0, 1]).rotate(Math.random() * 2 * PI);
     this.position = Vec.fromObject({ x, y });
   }
-  update() {
+  update(clip?: Vp[]) {
     this.velocity.add(this.acceleration);
-    if (this.velocity.magnitude() > MAX_FORCE) {
-      this.velocity.norm().mulScaler(MAX_FORCE);
+    if (this.velocity.magnitude() > MAX_SPEED) {
+      this.velocity.norm().mulScaler(MAX_SPEED);
     }
-    this.position.add(this.velocity);
+    let np = this.position.clone().add(this.velocity);
+    if (clip) {
+      if (polygonContains(clip as [number, number][], [np.x, np.y]))
+        this.position.add(this.velocity);
+      else {
+        this.velocity.mulScaler(0.5).rotate(Math.PI);
+      }
+    } else {
+      this.position.add(this.velocity);
+    }
     this.acceleration.mulScaler(0);
     return this;
   }
@@ -32,9 +58,9 @@ export class Node {
     const steer = new Vec([0, 0]);
     let count = 0;
     for (let other of nodes) {
-      let dist = this.position.dist(other.position);
-      if (dist > 0 && dist < DESIRED_SEPARATION) {
-        let diff = this.position.clone().sub(other.position).norm().divScaler(dist);
+      let dist = this.position.distSq(other.position);
+      if (dist > 0 && dist < DES_SEP_SQ) {
+        let diff = this.position.clone().sub(other.position).norm().divScaler(Math.sqrt(dist));
         steer.add(diff);
         count++;
       }
@@ -65,7 +91,7 @@ export class Node {
     let sum = new Vec([0, 0]);
     const lastIndex = nodes.length - 1;
     const thisIndex = nodes.indexOf(this);
-    if (thisIndex === -1) throw new Error(`Node is not present in set of nodes provided`);
+    if (thisIndex === -1) return new Vec([0, 0]);
     // begin the weird
     if (thisIndex !== 0 && thisIndex !== lastIndex) {
       sum.add(nodes[thisIndex - 1].position).add(nodes[thisIndex + 1].position);
@@ -77,34 +103,136 @@ export class Node {
     sum.divScaler(2);
     return this.seek(sum);
   }
-  differentiate(nodes: Array<Node>) {
-    let sepperation = this.sepparate(nodes);
+  differentiate(nodes: Array<Node>, seppNodes: Array<Node>) {
+    let sepperation = this.sepparate(seppNodes);
     let cohesion = this.edgeCohesion(nodes);
     sepperation.mulScaler(COHESION_RATIO);
     this.applyForce(sepperation);
     this.applyForce(cohesion);
     return this;
   }
-  run(nodes: Array<Node>) {
-    return this.differentiate(nodes).update();
+  run(nodes: Array<Node>, seppList: Array<Node>, clip?: Vp[]) {
+    this.differentiate(nodes, seppList);
+
+    this.update(clip);
+    return this;
   }
 }
 export class DifferentialLine extends Array<Node> {
-  constructor() {
+  quadtree: Quadtree<Node>;
+  clip: Vp[];
+  constructor(clip?: Vp[]) {
     super();
+    if (clip) this.clip = clip;
+    this.quadtree = quadtree<Node>()
+      .x((n) => n.position.x)
+      .y((n) => n.position.y);
   }
   run() {
-    for (let n of this) {
-      n.run(this);
+    for (let node of this) {
+      let nodes = search(
+        this.quadtree,
+        node.position.x + -10 * DESIRED_SEPARATION,
+        node.position.y + -10 * DESIRED_SEPARATION,
+        node.position.x + 10 * DESIRED_SEPARATION,
+        node.position.y + 10 * DESIRED_SEPARATION
+      );
+      node.run(this, nodes || [], this.clip);
     }
+    // this.refreshQuadTree();
     this.growth();
   }
   add(node: Node) {
     this.push(node);
+    this.quadtree.add(node);
     return this;
   }
   addAt(node: Node, index: number) {
     this.splice(index, 0, node);
+    this.quadtree.add(node);
+    return this;
+  }
+  differentiate() {
+    let seppForces = this.getSeppForces();
+    let cohesForces = this.edgeCohesion();
+    for (let i = 0; i < this.length; i++) {
+      let sepp = seppForces[i];
+      let cohes = cohesForces[i];
+      sepp.mulScaler(COHESION_RATIO);
+      let n = this[i];
+      n.applyForce(sepp);
+      n.applyForce(cohes);
+      n.update();
+    }
+  }
+  getSepperationForce(n1: Node, n2: Node) {
+    let p1 = n1.position;
+    let p2 = n2.position;
+    let steer = new Vec([0, 0]);
+    let sqd = Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2);
+    if (sqd > 0 && sqd < DES_SEP_SQ) {
+      let diff = p1.clone().sub(p2);
+      diff.norm().divScaler(Math.sqrt(sqd));
+      steer.add(diff);
+    }
+    return steer;
+  }
+  getSeppForces(): Vec[] {
+    let n = this.length;
+    let seppForces: Vec[] = [];
+    let nearNodes = [] as number[];
+    for (let i = 0; i < n; i++) {
+      seppForces[i] = new Vec([0, 0]);
+      nearNodes[i] = 0;
+    }
+    for (let i = 0; i < n; i++) {
+      let thisNode = this[i];
+      for (let j = i + 1; j < n; j++) {
+        let nextNode = this[j];
+        let forceIJ = this.getSepperationForce(thisNode, nextNode);
+        if (forceIJ.magnitude() > 0) {
+          seppForces[i].add(forceIJ);
+          seppForces[j].add(forceIJ);
+          nearNodes[i]++;
+          nearNodes[j]++;
+        }
+      }
+      if (nearNodes[i] > 0) {
+        seppForces[i].divScaler(nearNodes[i]);
+      }
+      if (seppForces[i].magnitude() > 0) {
+        seppForces[i].norm().mulScaler(MAX_SPEED);
+        seppForces[i].sub(this[i].velocity);
+        seppForces[i] = limitPure(seppForces[i], MAX_SPEED);
+      }
+    }
+    return seppForces;
+  }
+  edgeCohesion(): Vec[] {
+    let cohesionForces: Vec[] = [];
+    for (let i = 0; i < this.length; i++) {
+      const sum = new Vec([0, 0]);
+      const lastIndex = this.length - 1;
+      const thisIndex = i;
+      // begin the weird
+      if (thisIndex !== 0 && thisIndex !== lastIndex) {
+        sum.add(this[thisIndex - 1].position).add(this[thisIndex + 1].position);
+      } else if (thisIndex === 0) {
+        sum.add(this[lastIndex].position).add(this[thisIndex + 1].position);
+      } else if (thisIndex === lastIndex) {
+        sum.add(this[thisIndex - 1].position).add(this[0].position);
+      }
+      sum.divScaler(2);
+      cohesionForces[i] = this[thisIndex].seek(sum);
+    }
+    return cohesionForces;
+  }
+  refreshQuadTree() {
+    this.quadtree = quadtree<Node>()
+      .x((n) => n.position.x)
+      .y((n) => n.position.y)
+      .addAll(this);
+
     return this;
   }
   growth() {
--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,54 @@ import {
   catmulToBezier,
   drawBezierLoop,
   drawLine,
+  drawLoop,
   flr,
+  PI,
   rndm,
   spline,
+  TAU,
   Vec,
   Vp,
 } from '@rupertofly/h';
-import { range } from 'd3-array';
-import { hcl } from 'd3-color';
+import { mean, range } from 'd3-array';
+import { hcl, rgb } from 'd3-color';
+import * as PB from 'cli-progress';
 import * as col from './col';
 import { DifferentialLine, Node } from './diffGrowth';
-import capturer from '@rupertofly/capture-client';
-import { path } from 'd3';
+import walkPts from './points';
+// import capturer from '@rupertofly/capture-client';
+import { path, interpolateLab, scaleLinear } from 'd3';
 import { Path } from 'd3-path';
-const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
-const cap = new capturer(2469, canvas);
-canvas.width = 1080;
-canvas.height = 1920;
-const [W, H] = [canvas.width, canvas.height];
-const ctx = canvas.getContext('2d')!;
-ctx.fillStyle = col.darkGrey;
-ctx.fillRect(0, 0, W, H);
+import clip from './clip';
+import { writeFileSync } from 'fs';
+// const cap = new capturer(2469, canvas);
+
+const [W, H] = [297.64, 419.53];
+// const ctx = canvas.getContext('2d')!;
+// const
+// console.log(walkPts);
+
 const pts = range(30).map((i) => {
   let theta = (i / 30) * (Math.PI * 2);
   return new Vec([W / 2 + Math.cos(theta) * 40, H / 2 + Math.sin(theta) * 40]);
 });
-const line = new DifferentialLine();
-pts.forEach((pt) => {
-  line.add(new Node(pt.x, pt.y));
+const line = new DifferentialLine(clip);
+// range(32).forEach((i) => {
+//   let t = i / 32;
+//   let ang = t * TAU;
+//   line.add(new Node(W / 2 + Math.cos(ang) * 50, H / 2 + Math.sin(ang) * 50));
+// });
+walkPts.forEach((pt) => {
+  line.add(new Node(pt[0], pt[1]));
 });
+range(320).forEach((i) => line.growth());
 const cl = hcl(col.green);
-cap.start({
-  frameRate: 60,
-  lengthIsFrames: true,
-  maxLength: 600,
-  name: 'diff',
-});
+// cap.start({
+//   frameRate: 60,
+//   lengthIsFrames: true,
+//   maxLength: 600,
+//   name: 'diff',
+// });
 function customLoop(loop: [number, number][], close, ctx: CanvasRenderingContext2D) {
   for (let i = 0; i <= loop.length - 3; i += 4) {
     ctx.beginPath();
@@ -57,18 +69,42 @@ function customLoop(loop: [number, number][], close, ctx: CanvasRenderingContext
   }
   if (close) ctx.closePath();
 }
-async function render() {
-  ctx.fillStyle = col.darkGrey;
-  ctx.fillRect(0, 0, W, H);
+let fc = 0;
+let colInt = interpolateLab('#7c02ff', '#cca2ff');
+let cool = scaleLinear<string, string, never>()
+  .range([col.red, col.orange, col.yellow, col.green, col.cyan, col.purple, '#ff6188'])
+  .interpolate(interpolateLab)
+  .domain([0 / 7, 1 / 7, 2 / 7, 3 / 7, 4 / 7, 5 / 6, 6 / 6, 7 / 6]);
+const progBar = new PB.SingleBar({}, PB.Presets.shades_classic);
+const ITER_AMT = 500;
+console.log('Processing');
+
+progBar.start(ITER_AMT, 0);
+range(ITER_AMT).forEach((i) => {
   line.run();
-  ctx.lineCap = 'round';
-  ctx.lineWidth = 5;
-  ctx.strokeStyle = col.green;
-  ctx.fillStyle = col.green;
-  // const lp = [...spline(line.positions(), 3, false, flr(line.length * 3))];
-  ctx.beginPath();
-  customLoop(bezierSpline(line.positions(), 3, true) as any, false, ctx);
-  await cap.capture();
-  requestAnimationFrame(render);
+  progBar.increment();
+});
+progBar.stop();
+let outputPoints = line.positions();
+console.log('Drawing');
+let p = path();
+drawBezierLoop(catmulToBezier(outputPoints), true, p);
+console.log('drawn');
+const SVGHead = `<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 25.4.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="297.64px" height="419.53px" viewBox="0 0 297.64 419.53" style="enable-background:new 0 0 297.64 419.53;"
+	 xml:space="preserve">`;
+const svgPath = `<path fill="${col.green}" d="${p.toString()}" />`;
+writeFileSync('./output.svg', `${SVGHead}${svgPath}</svg>`);
+console.log('Written');
+
+function update() {
+  // ctx.beginPath();
+  // drawLoop(clip, true, ctx);
+  // ctx.stroke();
+  line.run();
+  let pts = line.positions();
+
+  fc++;
 }
-render();
